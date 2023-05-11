c7aa7a3e4beea896ee84e677267b9b08240bfdf3
carlamarquinaa <133105763+carlamarquinaa@users.noreply.github.com>
2023-05-11 17:19:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+import snowflake.connector
+
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
@@ -35,4 +37,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+
